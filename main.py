a1bd12f411b08fe8005ad52c7aea7e8f19c58c65
Denis Hohlov <deniss.hohlov@gmail.com>
2022-09-15 11:27:35 +0300

    second commit

--- main.py
+++ main.py
@@ -1,9 +1,14 @@
-#app/main.py
+# app/main.py
 
 from flask import Flask
 
 app = Flask(__name__)
 
+
 @app.route("/")
 def home_view():
-        return "<h1>Hello World!</h1>"
+    return "<h1>Hello World!</h1>"
+
+
+if __name__ == "__main__":
+    app.run()
--- wsgi.py
+++ wsgi.py
@@ -2,5 +2,3 @@
 
 from app.main import app
 
-if __name__ == "__main__":
-        app.run()
